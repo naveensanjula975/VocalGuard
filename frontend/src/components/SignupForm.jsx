36192fc81b72e014a1e0efc644bbeb2fe98e8450
Piumalhk <piumal.bcc@gmail.com>
2025-03-18 23:43:13 +0530

    Create SignupForm.jsx

---
+++ frontend/src/components/SignupForm.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../styles/signup.css";
+
+export default function SignupForm() {
+  return (
+    <div className="signup-page">
+      <div className="signup-container">
+        <div className="header2">
+          <h3 className="welcome-text2">Welcome👋</h3>
+          <h2 className="h">Sign up to your account</h2>
+        </div>
+
+        <form className="form2">
+          <label htmlFor="email" className="labb">Your Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            required
+          />
+
+          <label htmlFor="password" className="lab">Enter New Password</label>
+          <input
+            type="Password"
+            id="CPassword"
+            placeholder="Create password"
+            required
+          />
+          <label htmlFor="password" className="labb">Re Enter Password</label>
+          <input
+            type="password"
+            id="confirmpassword"
+            placeholder="Confirm password"
+            required
+          />
+
+          <button type="submit">SIGNUP</button>
+        </form>
+
+        <div className="forgot-password">
+          <a href="#">Forgot your password?</a>
+        </div>
+
+        <div className="signup-wrapper">
+          <div className="signup-container">
+            <p>
+              You have an account? <a href="#">Login</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
