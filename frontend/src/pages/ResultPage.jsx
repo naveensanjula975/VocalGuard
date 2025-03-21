292859ef4ff9a53c194ef8c63cb2f391528a1591
Piumalhk <piumal.bcc@gmail.com>
2025-03-21 11:53:36 +0530

    Update ResultPage.jsx

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Result from '../components/Result';
 
 export default function ResultPage() {
   return (
     <div>
       <NavBar/>
+      <Result/>
     </div>
   );
 };
