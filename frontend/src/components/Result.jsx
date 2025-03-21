ee8d2d47b3358fc73877b859d0b81a66c3062083
Piumalhk <piumal.bcc@gmail.com>
2025-03-21 12:01:28 +0530

    Create Result.jsx

---
+++ frontend/src/components/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/ResultStyle.css";
+
+
+export default function Result() {
+  return (
+  
+    <h1 className='head-text'>Audio Analysis Results</h1>
+ 
+  );
+}
