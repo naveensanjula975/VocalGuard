91a4207b3006421754fc97503feb9ccac16c6c3e
Piumalhk <piumal.bcc@gmail.com>
2025-03-22 16:08:44 +0530

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,26 +1,28 @@
-import React from "react";
-import "../styles/NavBar.css";
 
-export default function NavBar() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+import logo from '../assets/logoo.png';
+
+
+const Navbar = () => {
   return (
-    <header className="head">
-      <img className="logo" src="logo.jpg" alt="Logo" />
-
-      <nav className="navbar">
-        <a href="#" className="home">
-          Home
-        </a>
-        <span className="line">|</span>
-
-        <a href="#" className="about">
-          About Us
-        </a>
-
-        <span className="line">|</span>
-        <a href="#" className="history">
-          History
-        </a>
-      </nav>
-    </header>
+    <nav className="navbar">
+      <div className="nav-container">
+        <Link to="/" className="nav-logo">
+          <img src={logo} alt="VocalGuard" />
+        </Link>
+        
+        <div className="nav-links">
+          <Link to="/" className="nav-link">Home</Link>
+          <span className="separator">|</span>
+          <Link to="/about" className="nav-link">About us</Link>
+          <span className="separator">|</span>
+          <Link to="/history" className="nav-link">History</Link>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
