249cf930e505a7e61d53fb459649a3a5dfd38451
naveen sanjula <82176749+naveensanjula975@users.noreply.github.com>
2025-04-13 17:40:54 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
-import logo from '../assets/logo.png';
-
-=======
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import logo from "../assets/logoo.png";
->>>>>>> Stashed changes
 
 const Navbar = () => {
   return (
@@ -18,15 +10,6 @@ const Navbar = () => {
         <Link to="/" className="nav-logo">
           <img src={logo} alt="VocalGuard" />
         </Link>
-<<<<<<< Updated upstream
-        
-        <div className="nav-links">
-          <Link to="/" className="nav-link">Home</Link>
-          <span className="separator">|</span>
-          <Link to="/about" className="nav-link">About us</Link>
-          <span className="separator">|</span>
-          <Link to="/history" className="nav-link">History</Link>
-=======
 
         <div className="nav-links">
           <Link to="/" className="nav-link">
@@ -40,7 +23,6 @@ const Navbar = () => {
           <Link to="/history" className="nav-link">
             History
           </Link>
->>>>>>> Stashed changes
         </div>
       </div>
     </nav>
